7d8555a8523569c5005ad22f0c4d8c1ec0f47d47
AURORA GONZALEZ VIDAL <aurora.gonzalez2@um.es>
2018-09-26 18:10:00 +0200

    0alf

--- Alfonso/markovChain-i.R
+++ Alfonso/markovChain-i.R
@@ -1,4 +1,39 @@
 library("markovchain")
+library("data.table")
+df <- fread("TsetCleanV7.csv", sep=";")
+
+fillNAgaps <- function(x, firstBack=FALSE) {
+  ## NA's in a vector or factor are replaced with last non-NA values
+  ## If firstBack is TRUE, it will fill in leading NA's with the first
+  ## non-NA value. If FALSE, it will not change leading NA's.
+  
+  # If it's a factor, store the level labels and convert to integer
+  lvls <- NULL
+  if (is.factor(x)) {
+    lvls <- levels(x)
+    x    <- as.integer(x)
+  }
+  
+  goodIdx <- !is.na(x)
+  
+  # These are the non-NA values from x only
+  # Add a leading NA or take the first good value, depending on firstBack   
+  if (firstBack)   goodVals <- c(x[goodIdx][1], x[goodIdx])
+  else             goodVals <- c(NA,            x[goodIdx])
+  
+  # Fill the indices of the output vector with the indices pulled from
+  # these offsets of goodVals. Add 1 to avoid indexing to zero.
+  fillIdx <- cumsum(goodIdx)+1
+  
+  x <- goodVals[fillIdx]
+  
+  # If it was originally a factor, convert it back
+  if (!is.null(lvls)) {
+    x <- factor(x, levels=seq_along(lvls), labels=lvls)
+  }
+  
+  x
+}
 
 mEt <- array(NA, c(10, 10, ncol(df)))  
 mUt <- array(NA, c(10, 10, ncol(df)))  
@@ -51,3 +86,18 @@ writeMat("mLt.mat",mLt= mLt)
 
 
 
+
+x= mEt[1,,1]
+x2 = x
+library("fitdistrplus")
+
+plotdist(x, histo = TRUE, demp = TRUE)
+fw <- fitdist(x2[-c(7:10)], "weibull")
+fg <- fitdist(x2, "gamma")
+fln <- fitdist(x2, "lnorm")
+plot.legend <- c("Weibull", "lognormal", "gamma")
+denscomp(list(fw, fln, fg), legendtext = plot.legend)
+
+summary(fw)
+summary(fg)
+summary(fln)
